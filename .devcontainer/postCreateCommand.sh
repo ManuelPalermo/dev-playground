fa5fe855420e069ae22fd83873ed4729211f7ddc
ManuelPalermo <macpalermo@gmail.com>
2023-07-02 21:26:15 +0000

    setup developer env:
    docker based dev container with ubuntu22.04, nvidia-cuda11.7, python tools, shell tools, miniconda, camera and screen (X11) access
    devcontainer.json for vscode setup
    .vscode/setting.json

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+set -eo pipefail
+
+HOME=/home/${USER}
+#WS_PATH="${HOME}/workspace"
+#cd "${WS_PATH}"
+#sudo chown -R "${USER}" "${HOME}"
+
+echo ">> Updating apt-get package list"
+sudo apt-get update
+
+echo ">> Setting up miniconda"
+source "${HOME}/miniconda3/bin/activate"
+conda init bash
+conda deactivate
+conda config --set auto_activate_base false
+conda update -n base -c defaults conda -y
+
+echo ">> Installing pre-commit hooks"
+pre-commit install && pre-commit autoupdate
